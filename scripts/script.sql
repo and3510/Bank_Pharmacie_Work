cf27b7043c03ae2b982690baced2b1174f370766
anderson <and.dovale@gmail.com>
2024-08-29 14:43:37 -0300

    primeiro script

---
+++ scripts/script.sql
@@ -0,0 +1,23 @@
+create database farmacia
+
+use database farmacia;
+
+
+/* 
+
+CRIACAO DAS TABELAS NO BANCO DE DADOS:
+
+create table clientes()
+create table fornecedores()
+create table vendas()
+create table funcionarios()
+create table prescricoes_medicas()
+create table planos_saude()
+create table produtos()
+create table servicos()
+create table lojas(); 
+
+
+
+
+*/
